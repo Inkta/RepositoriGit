2cbb898c27c649588785fed2ef76ef164553f909
marc <mquerluque@gmail.com>
2014-10-10 19:30:36 +0200

    Primer

---
+++ projecte.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class exercici {
+
+	public static void main(String[] args) {
+		Scanner lector=new Scanner (System.in);
+		System.out.println("Escriu una frase: ");
+		String frase=lector.nextLine();
+	        char [] array=frase.toCharArray();
+        	for (int i=0;i<frase.length();i++){
+        		if (array[i] != ' '){
+        			int contador=contador(frase,array[i]);
+            		System.out.println("la lletra "+array[i]+ " surt "+contador);
+        		}
+        	}
+ 
+        }
+
+	public static int contador(String frase, char c){
+	int contador=0;
+	for (int i=0;i<frase.length();i++){
+			if (frase.charAt(i) == c){
+				contador++;
+			}
+		}
+	return contador;
+	}
+}
